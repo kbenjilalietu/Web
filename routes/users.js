d905e362bc8b8f055790e99313bf380535557793
BENJILALI Khadija <k.benjilali_etu@enset-media.ac.ma>
2021-06-11 12:45:39 +0100

    question 16

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const router = require('express').Router();
+const usersRepo = require('../repositories/users')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+ /* GET users listing. */
+ router.get('/', async function(req, res, next) {
 
-module.exports = router;
+   res.send(await usersRepo.getAllUsers())
+
+ });
+
+ module.exports = router;
+
